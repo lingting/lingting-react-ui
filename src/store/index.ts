917e160a5abfb4f08be41c75a6f245bd9bae7e17
lingting <sunlisten.gzm@foxmail.com>
2026-08-16 21:24:07 +0800

    :bug: 修复 store 未导出类型

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,8 @@
 export { UserStore, useUserStore } from "./userStore";
+export type {
+  AuthRule,
+  ProUser,
+  ProUserAction,
+  ProUserStoreInitializeOptions,
+  UseUserStoreResult,
+} from "@lri//types";
