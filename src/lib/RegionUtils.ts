89d9a537a2adb63d09ac7f299dd2abada062a3d3
lingting <sunlisten.gzm@foxmail.com>
2026-08-31 15:11:53 +0800

    :sparkle: 扩展导出区域数据类型

--- src/lib/RegionUtils.ts
+++ src/lib/RegionUtils.ts
@@ -1,12 +1,25 @@
-import type { RegionFilter, RegionItem } from "@lri/types";
+import type { RegionFilter, RegionItem, RegionPhone } from "@lri/types";
 
 import regionM49Data from "../components/region/m49.json";
 import regionPhoneData from "../components/region/phones.json";
 import regionData from "../components/region/regions.json";
 
 export const regionItems = regionData as RegionItem[];
+
 export const regionM49 = regionM49Data;
-export const regionPhones = regionPhoneData;
+
+export const regionPhones = regionPhoneData as RegionPhone[];
+
+export const regionPhoneMap = regionPhones.reduce<
+  Record<string, { data: RegionPhone[]; calling: number[]; prefix: number[] }>
+>((map, phone) => {
+  const item = map[phone.region] ?? { data: [], calling: [], prefix: [] };
+  item.data.push(phone);
+  if (!item.calling.includes(phone.calling)) item.calling.push(phone.calling);
+  if (!item.prefix.includes(phone.prefix)) item.prefix.push(phone.prefix);
+  map[phone.region] = item;
+  return map;
+}, {});
 
 export const regionItemMap = new Map(regionItems.map((item) => [item.iso, item]));
 
