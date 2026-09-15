9303f509d4ead66333d7f8a4226c41d34c8e126f
lingting <sunlisten.gzm@foxmail.com>
2026-09-15 20:37:21 +0800

    :wrench: 允许设置render

--- src/types/ExFormTypes.ts
+++ src/types/ExFormTypes.ts
@@ -2,11 +2,9 @@ import type { ProFormFieldProps } from "@ant-design/pro-components";
 import type { RegionFilter, RegionRender } from "./RegionTypes";
 import type { DictProps, DictValue } from "./DictTypes";
 
-type ExFormDictFieldProps<T extends DictValue = DictValue> = Omit<
-  ProFormFieldProps,
-  "children" | "render" | "renderFormItem" | "valueType"
-> &
-  DictProps<T>;
+type OmitProFormFieldProps = Omit<ProFormFieldProps, "children" | "valueType">;
+
+type ExFormDictFieldProps<T extends DictValue = DictValue> = OmitProFormFieldProps & DictProps<T>;
 
 export type ExFormDictSelectProps<T extends DictValue = DictValue> = ExFormDictFieldProps<T>;
 export type ExFormDictCheckboxProps<T extends DictValue = DictValue> = ExFormDictFieldProps<T>;
@@ -14,10 +12,7 @@ export type ExFormDictBadgeProps<T extends DictValue = DictValue> = ExFormDictFi
 export type ExFormDictTagProps<T extends DictValue = DictValue> = ExFormDictFieldProps<T>;
 export type ExFormDictTextProps<T extends DictValue = DictValue> = ExFormDictFieldProps<T>;
 
-type ExFormRegionBaseProps = Omit<
-  ProFormFieldProps,
-  "children" | "render" | "renderFormItem" | "valueType"
-> & {
+type ExFormRegionBaseProps = OmitProFormFieldProps & {
   className?: string;
   filter?: RegionFilter;
   renderItem?: RegionRender;
